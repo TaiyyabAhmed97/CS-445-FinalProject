a54351e830ddb1574f3e1dd4b1420d8ca0a6da3f
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2017-11-06 20:08:00 -0600

    created order model

--- models/Order.js
+++ models/Order.js
@@ -5,7 +5,7 @@ class Order{
         this.oid = oid;
         this.wid = wid;
         this.show_info = show_info;
-        this. date_ordered = this.getdate(date_ordered);
+        this.date_ordered = this.getdate(date_ordered);
         this.order_amount = order_amount;
         this.number_of_tickets = number_of_tickets;
         this.patron_info = patron_info;
