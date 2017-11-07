18771fa98e2f7bc7bd3ba26e1fed4311db8a7950
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2017-11-07 16:26:22 -0600

    modified Order model

--- models/Order.js
+++ models/Order.js
@@ -1,19 +1,35 @@
 var moment = require('moment');
 class Order{
-    constructor(oid, wid, show_info, date_ordered, order_amount, number_of_tickets, patron_info)
+    constructor(oid, wid, show_info, order_amount, number_of_tickets, patron_info)
     {
-        this.oid = oid;
+        this.oid = Order.setoid();
         this.wid = wid;
         this.show_info = show_info;
-        this.date_ordered = this.getdate(date_ordered);
-        this.order_amount = order_amount;
-        this.number_of_tickets = number_of_tickets;
+        this.date_ordered = Order.setdate();
+        this.order_amount = Order.setamount(order_amount);
+        this.number_of_tickets = Order.setnum();
         this.patron_info = patron_info;
     }
-    getdate(date_ordered)
+    setdate()
     {
-        var momentdate = moment(date_ordered);
-        return momentdate;
+        if (!this.date_ordered) 
+        { this.date_ordered = moment().format("YYYY-MM-DD h:mm"); } 
+        return this.date_ordered;
+    }
+    static setoid()
+    {
+        if (!this.sid) 
+        { this.sid = 123; } 
+        else this.sid++; 
+        return this.sid; 
+    }
+    setamount(orders)
+    {
+        
+    }
+    setnum()
+    {
+
     }
 }
 module.exports = Order;
