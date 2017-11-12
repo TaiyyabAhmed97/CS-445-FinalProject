70aacc47185d6d1e94865d3e7cc9802dce5a7b89
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2017-11-11 19:46:27 -0600

    added request contiguous seating functionality

--- models/Theater.js
+++ models/Theater.js
@@ -1,8 +1,73 @@
+let Row = require('./Row.js');
 class Theater{
     constructor(show, sectholders, orders)
     {
         this.show = show;
         this.sectholders = sectholders;
+        this.orders = orders;
+    }
+    addOrder(order)
+    {
+        this.orders.push(Order);
+    }
+    getseating(wid, sid, count, start)
+    {
+        let sect = this.getSect(wid, sid);
+        //return sect;
+        let section_name = sect.name;
+        var row = new Row();
+        let sectseats = sect.seats;
+        if(!start)
+        {
+            let seating = [];
+            for(var i =0;i<sectseats.length;i++)
+            {
+                let rownum = sectseats[i];
+                let rowseats = sectseats[i].seats;
+                for(var j=0;j<rowseats.length;j++)
+                {
+                    if(seating.length!=count)
+                    {
+                        if(rowseats[j].status == 'available')
+                        {
+                            seating.push(rowseats[j]);
+                        }
+                        else
+                        {
+                            seating = [];
+                        }
+                    }
+                     else
+                    {
+                        break;
+                    }
+                }
+            }
+            let status = 'ok';
+            let show = this.getShowbyID(wid).getShow();
+            let total_amount = 0;
+            let sections = this.getShowbyID(wid).sections;
+            for(var k = 0;k<sections.length;k++)
+            {
+                if(sections[k].sid == sid)
+                {
+                    total_amount = sections[k].price * count;
+                    k = sections.length + 1;
+                }
+            }
+            let obj1 = Object.assign(show, sid);
+            obj1 = Object.assign(obj1, status);
+            obj1 = Object.assign(obj1, total_amount);
+            obj1 = Object.assign(obj1, seating);
+            return obj1;
+            
+         }
+         else
+         {
+
+         }
+
+
     }
     addShow(show)
     {
@@ -38,7 +103,7 @@ class Theater{
     {
         for( var i =0;i<this.sectholders.length;i++)
         {
-            console.log(this.sectholders[i]);
+            //console.log(this.sectholders[i]);
             if((this.sectholders[i].wid == wid) && (this.sectholders[i].sid == sid))
             {
                 return this.sectholders[i];
