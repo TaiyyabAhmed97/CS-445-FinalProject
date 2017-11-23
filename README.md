c6472878757039024a301fe339ce35ff1be09e97
Taiyyab Ahmed <hafiztaiyyab11@gmail.com>
2017-11-22 23:57:50 -0600

    finalchange

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 #CS -445-FinalProject
+to configure this app,
+first run the ./depends script in your working folder which will install all dependencies
+once the ./depends script is run, the project will now be listening on the 8080 localhost server
+to run the tests, restart the server and run the tests using the command
+nyc npm test
+also nyc npm test will give the code coverage of the tests.
+-known bugs are the last 2 API endpoints could not be implemented due to time constraints, the search and the Donations and Revenue Reports. Code Coverage is at 41.98% also due to time constraints
+Credits and Acknowledgements:
+I'd like to credit the professor, he did inform us that this would be a very long project and I definitely learnt alot.
+
--- Tests/ShowModeltest.js
+++ Tests/ShowModeltest.js
@@ -16,7 +16,136 @@ var util = require("util");
 let chai = require('chai');
 require("blanket");
 let chaiHttp = require('chai-http');
-
+let obj = {
+  "show_info": {
+    "name": "King Lear",
+    "web": "http://www.example.com/shows/king-lear",
+    "date": "2017-12-05",
+    "time": "13:00"
+  },
+  "seating_info": [
+    {
+      "sid": "123",
+      "price": 600
+    },
+    {
+      "sid": "124",
+      "price": 75
+    },
+    {
+      "sid": "125",
+      "price": 60
+    }
+  ]
+};
+let obj2 = {
+  "wid": 300,
+  "sid": "123",
+  "name": "Front right",
+  "price": 60,
+  "seating": [
+      {
+          "row": "1",
+          "seats": [
+              {
+                  "cid": 200,
+                  "seat": "1",
+                  "status": "available"
+              },
+              {
+                  "cid": 201,
+                  "seat": "2",
+                  "status": "available"
+              },
+              {
+                  "cid": 202,
+                  "seat": "3",
+                  "status": "available"
+              },
+              {
+                  "cid": 203,
+                  "seat": "4",
+                  "status": "available"
+              }
+          ]
+      },
+      {
+          "row": "2",
+          "seats": [
+              {
+                  "cid": 204,
+                  "seat": "1",
+                  "status": "available"
+              },
+              {
+                  "cid": 205,
+                  "seat": "2",
+                  "status": "available"
+              },
+              {
+                  "cid": 206,
+                  "seat": "3",
+                  "status": "available"
+              },
+              {
+                  "cid": 207,
+                  "seat": "4",
+                  "status": "available"
+              }
+          ]
+      },
+      {
+          "row": "3",
+          "seats": [
+              {
+                  "cid": 208,
+                  "seat": "1",
+                  "status": "available"
+              },
+              {
+                  "cid": 209,
+                  "seat": "2",
+                  "status": "available"
+              },
+              {
+                  "cid": 210,
+                  "seat": "3",
+                  "status": "available"
+              },
+              {
+                  "cid": 211,
+                  "seat": "4",
+                  "status": "available"
+              }
+          ]
+      },
+      {
+          "row": "4",
+          "seats": [
+              {
+                  "cid": 212,
+                  "seat": "1",
+                  "status": "available"
+              },
+              {
+                  "cid": 213,
+                  "seat": "2",
+                  "status": "available"
+              },
+              {
+                  "cid": 214,
+                  "seat": "3",
+                  "status": "available"
+              },
+              {
+                  "cid": 215,
+                  "seat": "4",
+                  "status": "available"
+              }
+          ]
+      }
+  ]
+};
 describe('Show.js functions', function()
 {
     it('getwid()', function(){
@@ -33,10 +162,66 @@ describe('Show.js functions', function()
     }});
     });
 });
-describe('Theater.js functions', function()
+describe('Seat.js functions', function()
 {
-    it('getseating()', function(){
-      let Theater1 = new Theater();
-      let show1 = new Show();
+    it('seat()', function(){
+      let set = new Seat();
+      set.sold();
+      expect(set.status).equals('available');
+    });
+    it('seat1()', function(){
+      let set = new Seat();
+      set.unsold();
+      expect(set.status).equals('available');
     });
   });
+
+  describe('theater.js functions', function()
+  {
+      it('sroweat()', function(){
+        let theater = new Theater();
+        theater.show = [];
+        theater.sectholders = [];
+        theater.show.push(obj);
+        theater.sectholders.push(obj2);
+        expect(theater.getseating(300, 123, 3)).to.deep.equal({
+          "wid": "300",
+          "show_info": {
+              "name": "King Lear",
+              "web": "http://www.example.com/shows/king-lear",
+              "date": "2017-12-05",
+              "time": "13:00"
+          },
+          "sid": "123",
+          "section_name": "Front right",
+          "starting_seat_id": 200,
+          "status": "ok",
+          "total_amount": 180,
+          "seating": {
+              "row": "1",
+              "seats": [
+                  {
+                      "cid": 200,
+                      "seat": "1",
+                      "status": "available"
+                  },
+                  {
+                      "cid": 201,
+                      "seat": "2",
+                      "status": "available"
+                  },
+                  {
+                      "cid": 202,
+                      "seat": "3",
+                      "status": "available"
+                  }
+              ]
+          }
+      })
+      });
+      it('row()', function(){
+        let set = new Seat();
+        set.unsold();
+        expect(set.status).equals('available');
+      });
+    });
